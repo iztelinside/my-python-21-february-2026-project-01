6c2377b0a13f4f44a710d36246fb82f39dd19789
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-22 17:47:12 +0500

    first massive commit_01

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 
-number = 100
-text = "Hello World!"
-print(number)
-print(text)
-for i in text:
-    # print(i)
-    if i == '!':
-        print(i)
-    elif i == 'b':
-        print(i)
+# number = 100
+# text = "Hello World!"
+# print(number)
+# print(text)
+# for i in text:
+#     # print(i)
+#     if i == '!':
+#         print(i)
+#     elif i == 'b':
+#         print(i)
+
+listNumbers = [100, 60, 70, 80, 500]
+print(listNumbers)
+print(len(listNumbers))
+print(max(listNumbers))
+print(min(listNumbers))
+print(sum(listNumbers))
+print(listNumbers[0])
+print(listNumbers[-1])
+print(listNumbers[2])
+listNumbers.append(1000)
+print(listNumbers)
+listNumbers.remove(500)
+print(listNumbers)
+print(sum(listNumbers))
+listNumbers[2] = True
+print(listNumbers)
+listNumbers[3] = "Hello World"
+print(listNumbers)
+print(len(listNumbers))
+
+listNumbers.insert(2, 105)
+print(listNumbers)
+
+len = int(input("Enter the length of the list: "))
+element_list = []
+i = 0
+while i < len:
+    string = input("Enter the element #"+ str(i+1)+ ":")
+    element_list.append(string)
+    # element_list.append(element_list[i])
+    i += 1
+print(element_list)
