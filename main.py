76b282af2bad3edaac519b4ee090af37013a6add
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-23 20:36:09 +0500

    first function workFileswithPython_01_01

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-def createWorst(elementOne, elementTwo):
-    print("Hello " + elementOne + " " + elementTwo)
+# data = input("Enter Your Text:")
 
 
-createWorst(elementOne="John", elementTwo="Black")
-
-def sumExam(elOne, elTwo, elThree, elFour):
-    res = elOne + elTwo + elThree+ elFour
-    return res
-
-print(sumExam(0, 5.5, 6.5, 10))
+file = open("data/text.txt", "w")
+file.write("Hello World")
+file.close()
 
 
-numbers = [10, 90, 5, 39, -6, 67, 100, 400]
-min = numbers[0]
-for number in numbers:
-    if number < min:
-        min = number
+# def createWorst(elementOne, elementTwo):
+#     print("Hello " + elementOne + " " + elementTwo)
 
-print(min)
-max = numbers[0]
-for number in numbers:
-    if number > max:
-        max = number
-print(max)
+
+# createWorst(elementOne="John", elementTwo="Black")
+#
+# def sumExam(elOne, elTwo, elThree, elFour):
+#     res = elOne + elTwo + elThree+ elFour
+#     return res
+#
+# print(sumExam(0, 5.5, 6.5, 10))
+#
+#
+# numbers = [10, 90, 5, 39, -6, 67, 100, 400]
+# min = numbers[0]
+# for number in numbers:
+#     if number < min:
+#         min = number
+#
+# print(min)
+# max = numbers[0]
+# for number in numbers:
+#     if number > max:
+#         max = number
+# print(max)
