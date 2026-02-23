31187f3b448de636f4c583c8762be436766e7a02
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-23 20:00:22 +0500

    first function commit_01_02

--- main.py
+++ main.py
@@ -1,100 +1,25 @@
-data = set("hello world")
-print(data)
+def createWorst(elementOne, elementTwo):
+    print("Hello " + elementOne + " " + elementTwo)
 
-numbers = 4, 6, 7, 9, 9, 6
-data2 = set(numbers)
-data3 = set(numbers)
-print(data2)
-data2.add(1)
-print(data2)
-data3.update([10, 45, 50, True, "34"])
-print(data3)
-data3.remove(10)
-print(data3)
-data3.pop()
-print(data3)
-country = {"code": "Ru", "name": "Russia", "population": 31500}
-print(country["name"])
-print(country.get("name"))
-print(country.clear())
-country.pop("name")
-print(country)
-country.clear()
-print(country)
-print(type(country))
-country2 = dict(country)
-print(country2)
-country3 = dict(code = "RU", name = "Russia", population = 60000)
-print(country3)
-print(country.get("name"))
-for key, value in country.items():
-    print({key : value})
-    print(key, value)
-    print(key, "--" , value)
-data = (100, 60, 70, 80, 500)
-print(type(data))
-print(len(data))
-print(max(data))
-print(min(data))
-print(sum(data))
-print(data)
 
-from itertools import count
+createWorst(elementOne="John", elementTwo="Black")
 
-number = 100
-text = "Hello World!"
-print(number)
-print(text)
-for i in text:
-    # print(i)
-    if i == '!':
-        print(i)
-    elif i == 'b':
-        print(i)
+def sumExam(elOne, elTwo, elThree, elFour):
+    res = elOne + elTwo + elThree+ elFour
+    return res
 
-listNumbers = [100, 60, 70, 80, 500]
-print(listNumbers)
-print(len(listNumbers))
-print(max(listNumbers))
-print(min(listNumbers))
-print(sum(listNumbers))
-print(listNumbers[0])
-print(listNumbers[-1])
-print(listNumbers[2])
-listNumbers.append(1000)
-print(listNumbers)
-listNumbers.remove(500)
-print(listNumbers)
-print(sum(listNumbers))
-listNumbers[2] = True
-print(listNumbers)
-listNumbers[3] = "Hello World"
-print(listNumbers)
-print(len(listNumbers))
+print(sumExam(0, 5.5, 6.5, 10))
 
-listNumbers.insert(2, 105)
-print(listNumbers)
 
-len = int(input("Enter the length of the list: "))
-element_list = []
-i = 0
-while i < len:
-    string = input("Enter the element #"+ str(i+1)+ ":")
-    element_list.append(string)
-    # element_list.append(element_list[i])
-    i += 1
-print(element_list)
-textSlogan = "Power is nothing without control"
-slogan = textSlogan.split(" ")
-print(slogan)
-i = 0
-for i in range(len(slogan)):
-    slogan[i] = slogan[i].capitalize()
-print(slogan)
-data = (100, 60, 70, 80, 500)
-print(type(data))
-print(len(data))
-print(max(data))
-print(min(data))
-print(sum(data))
-print(data)
+numbers = [10, 90, 5, 39, -6, 67, 100, 400]
+min = numbers[0]
+for number in numbers:
+    if number < min:
+        min = number
+
+print(min)
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
