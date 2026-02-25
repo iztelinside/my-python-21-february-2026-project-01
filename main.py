71f858ef66b6c55cb396e65206cb2f27aaa082d0
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-25 16:34:57 +0500

    Creating and working oop-class_01_01

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
+#17 – Основы ООП. Создание класса и объекта
+class Person:
+    name  = None
+    age = None
+    isMale = True
+
+    def self_data(self, name, age, isMale):
+        self.name = name
+        self.age = age
+        self.isMale = isMale
+    def get_data(self, name, age, isMale):
+        print(self.name, "age:", self.age, "isMale: ",self.isMale)
+
+personSelf = Person()
+personSelf.self_data('Red', 26, True)
+personSelf.self_data('John', 26, True)
+personSelf.get_data('John', 26, True)
+print(personSelf.name)
+print(personSelf.age)
+person1 = Person()
+person1.name = "Dan"
+person1.age = 20
+person1.isMale = True
+print(person1.name)
+print(person1.age)
+
+
+person2 = Person()
+person2.name = "Victoria"
+person2.age = 30
+person2.isMale = False
+print(person2.name)
+print(person2)
+
+    # def __init__(self, name, age):
+    #     self.name = name
+    #     self.age = age
+
+
 # module
 
-import datetime as dt
-print(dt.datetime.today())
-print(dt.datetime.now().time())
-print(dt.datetime.now())
-
-from math import sqrt as sq
-from math import pi as pi
-print(sq(2))
-print(pi)
+# import datetime as dt
+# print(dt.datetime.today())
+# print(dt.datetime.now().time())
+# print(dt.datetime.now())
+#
+# from math import sqrt as sq
+# from math import pi as pi
+# print(sq(2))
+# print(pi)
 # def createWorst(elementOne, elementTwo):
 #     print("Hello " + elementOne + " " + elementTwo)
 
